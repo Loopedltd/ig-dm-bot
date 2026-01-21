f2de3bfdd09ea36575131b3f40f248a0c22d3337
James Binks <jamesbinks@Jamess-Air.home.gateway>
2026-01-21 13:40:20 +0000

    Improve env var error message

--- supabaseClient.js
+++ supabaseClient.js
@@ -7,7 +7,7 @@ const url = process.env.SUPABASE_URL;
 const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!url || !serviceKey) {
-  throw new Error("Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY in .env");
+  throw new Error("Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY in environment variables");
 }
 
 export const supabase = createClient(url, serviceKey, {
